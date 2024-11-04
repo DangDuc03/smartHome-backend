26769b79578fa9a60d33a74e52d6b80782853e57
DANG DUC <dangcongduc38@gmail.com>
2024-11-05 00:44:28 +0700

    update POST

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
